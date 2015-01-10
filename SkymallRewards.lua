20c01a6df93cad61663b381b58511f2399dff7e4
Joe Fredette <jfredett@gmail.com>
2015-01-10 02:24:31 -0500

    rough in addon object creation and mixins

---
+++ SkymallRewards.lua
@@ -0,0 +1,11 @@
+SkymallRewards = LibStub("AceAddon-3.0"):NewAddon(
+  "SkymallRewards",
+  "AceConsole-3.0",
+  "AceConfig-3.0",
+  "AceDB-3.0",
+  "AceEvent-3.0",
+  "AceComm-3.0",
+  "AceHook-3.0",
+  "AceSerializer-3.0",
+  "AceGUI-3.0"
+)
